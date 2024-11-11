b492087a8fb8c10743dce65b4de07d74617e869e
Ajesh <ajeshs.dev@gmail.com>
2024-11-12 00:15:32 +0530

    Image fallback implemented in vehicle details page

--- src/components/root/vehicle details/vehicle-images/Images.tsx
+++ src/components/root/vehicle details/vehicle-images/Images.tsx
@@ -1,7 +1,7 @@
-'use client'
-import './Images.scss'
+"use client";
+import "./Images.scss";
 
-import Autoplay from 'embla-carousel-autoplay'
+import Autoplay from "embla-carousel-autoplay";
 import {
   Carousel,
   CarouselContent,
@@ -9,33 +9,33 @@ import {
   CarouselNext,
   CarouselPrevious,
   type CarouselApi,
-} from '@/components/ui/carousel'
-import { useEffect, useRef, useState } from 'react'
-import Image from 'next/image'
-import MotionDiv from '@/components/general/framer-motion/MotionDiv'
+} from "@/components/ui/carousel";
+import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
+import MotionDiv from "@/components/general/framer-motion/MotionDiv";
 
 type ImagesProps = {
-  photos: string[]
-}
+  photos: string[];
+};
 
 const Images = ({ photos }: ImagesProps) => {
-  const [api, setApi] = useState<CarouselApi>()
-  const [current, setCurrent] = useState(0)
-  const [count, setCount] = useState(0)
-  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: false }))
+  const [api, setApi] = useState<CarouselApi>();
+  const [current, setCurrent] = useState(0);
+  const [count, setCount] = useState(0);
+  const plugin = useRef(Autoplay({ delay: 3000, stopOnInteraction: false }));
 
   useEffect(() => {
     if (!api) {
-      return
+      return;
     }
 
-    setCount(api.scrollSnapList().length)
-    setCurrent(api.selectedScrollSnap() + 1)
+    setCount(api.scrollSnapList().length);
+    setCurrent(api.selectedScrollSnap() + 1);
 
-    api.on('select', () => {
-      setCurrent(api.selectedScrollSnap() + 1)
-    })
-  }, [api])
+    api.on("select", () => {
+      setCurrent(api.selectedScrollSnap() + 1);
+    });
+  }, [api]);
 
   return (
     <MotionDiv className="images-container">
@@ -46,7 +46,7 @@ const Images = ({ photos }: ImagesProps) => {
         onMouseEnter={plugin.current.stop}
         onMouseLeave={() => plugin.current.play()}
         opts={{
-          align: 'start',
+          align: "start",
         }}
       >
         <CarouselContent className="gap-2 -mx-1 w-full h-full max-w-full sub-container">
@@ -56,14 +56,20 @@ const Images = ({ photos }: ImagesProps) => {
                 key={index}
                 className="w-full min-w-[100%] rounded-[1rem] relative p-0  overflow-hidden"
               >
-                <Image
-                  src={src}
-                  alt={`Vehicle image ${index + 1}`}
-                  className="w-full h-full object-contain rounded-[1rem]"
-                  fill
-                />
+                {src ? (
+                  <Image
+                    src={src}
+                    alt={`Vehicle image ${index + 1}`}
+                    className="w-full h-full object-contain rounded-[1rem]"
+                    fill
+                  />
+                ) : (
+                  <div className="w-full text-lg text-slate-500 h-full object-contain flex-center rounded-[1rem] ">
+                    Oops! Image loading failed!
+                  </div>
+                )}
               </CarouselItem>
-            )
+            );
           })}
         </CarouselContent>
         <CarouselPrevious className="max-md:hidden ml-[2.8rem]" />
@@ -74,12 +80,12 @@ const Images = ({ photos }: ImagesProps) => {
         {Array.from({ length: count }).map((_, index) => (
           <div
             key={index}
-            className={`dot ${current - 1 === index ? 'active' : ''}`}
+            className={`dot ${current - 1 === index ? "active" : ""}`}
           ></div>
         ))}
       </div>
     </MotionDiv>
-  )
-}
+  );
+};
 
-export default Images
+export default Images;
