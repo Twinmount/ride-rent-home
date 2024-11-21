eba5715ae1d7c682f165db07416b941ab1a0c920
Ajesh <ajeshs.dev@gmail.com>
2024-11-21 21:36:46 +0530

    features bug fixed, description toggle enabled

--- src/app/api/indexnow/route.ts
+++ src/app/api/indexnow/route.ts
@@ -1,31 +1,50 @@
-import { submitUrlsToIndexNow } from "@/utils/indexNow";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST(req: NextRequest) {
   try {
-    const body = await request.json();
+    const { urlList } = await req.json();
 
-    if (!body.urlList || !Array.isArray(body.urlList)) {
+    if (!Array.isArray(urlList) || urlList.length === 0) {
       return NextResponse.json(
-        { message: "Invalid request. urlList must be an array." },
+        { error: "urlList is required and must be an array" },
         { status: 400 }
       );
     }
 
-    const urlList = body.urlList;
+    const API_ENDPOINT = "https://api.indexnow.org/indexnow";
+    const INDEX_NOW_KEY = process.env.NEXT_PUBLIC_INDEX_NOW_KEY;
+    const DOMAIN = process.env.NEXT_PUBLIC_HOST;
+    const KEY_LOCATION = `${DOMAIN}/${INDEX_NOW_KEY}.txt`;
 
-    // Call the helper function to submit URLs
-    const result = await submitUrlsToIndexNow(urlList);
+    const payload = {
+      host: DOMAIN,
+      key: INDEX_NOW_KEY,
+      keyLocation: KEY_LOCATION,
+      urlList,
+    };
+
+    const response = await fetch(API_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      body: JSON.stringify(payload),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      return NextResponse.json(
+        { error: errorText },
+        { status: response.status }
+      );
+    }
 
     return NextResponse.json(
-      { message: "URLs submitted successfully", result },
+      { message: "URLs submitted successfully" },
       { status: 200 }
     );
-  } catch (error) {
-    console.error("Error submitting URLs:", error);
-    return NextResponse.json(
-      { message: "Error submitting URLs", error },
-      { status: 500 }
-    );
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    return NextResponse.json({ error: errorMessage }, { status: 500 });
   }
 }
