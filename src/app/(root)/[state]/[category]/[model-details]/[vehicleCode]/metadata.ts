f3dc9127d459746dc21f91912ddd6c8c94a060f0
Ajesh <ajeshs.dev@gmail.com>
2025-01-14 19:29:07 +0530

    details page meta data title updated

--- src/app/(root)/[state]/[category]/[model-details]/[vehicleCode]/metadata.ts
+++ src/app/(root)/[state]/[category]/[model-details]/[vehicleCode]/metadata.ts
@@ -48,7 +48,7 @@ export function generateVehicleMetadata(
   const vehicle = data.result;
 
   // Construct the title
-  const title = `Rent ${vehicle.vehicleTitle || category} | Starts 24 AED/Day ${singularizeType(convertToLabel(category))} Rentals in  ${convertToLabel(state)}`;
+  const title = `Rent ${vehicle.vehicleTitle || category} | Starts 34 AED/Day ${singularizeType(convertToLabel(category))} Rentals in  ${convertToLabel(state)}`;
 
   // Construct the description
   const description = `${vehicle.vehicleTitle || category} For Rent in ${convertToLabel(
