a852db99ba05c4a4222304b2cb765475c23edb2a
arunraj.r <arunraj.r@nergyindia.com>
2025-06-21 15:34:55 +0530

    link-added to footer

--- src/components/footer/footer-grid/index.ts
+++ src/components/footer/footer-grid/index.ts
@@ -11,11 +11,18 @@ export const locations = [
 
 export const company = [
   { id: 1, title: "About Us", link: "/about-us" },
-  { id: 2, title: "List Vehicles", link: "https://agent.ride.rent/ae/register", linkIndia:'https://agent.ride.rent/in/register' },
+  {
+    id: 2,
+    title: "List Vehicles",
+    link: "https://agent.ride.rent/ae/register",
+    linkIndia: "https://agent.ride.rent/in/register",
+  },
   { id: 3, title: "Privacy Policy", link: "/privacy-policy" },
   { id: 4, title: "Terms & Conditions", link: "/terms-condition" },
   { id: 5, title: "FAQ", link: "/faq" },
   { id: 6, title: "Our Blogs", link: "/blog" },
+  { id: 7, title: "Careers", link: "/ae/careers", linkIndia: "/in/careers" },
+  { id: 78, title: "Interns", link: "/ae/interns", linkIndia: "/in/interns" },
 ];
 
 export const vehicles = [
