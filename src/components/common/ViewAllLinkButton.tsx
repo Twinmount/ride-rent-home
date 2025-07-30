81fbd69baaf48e8279b007645025fd40895a9561
adithyan7053 <adithyan7053@gmail.com>
2025-07-30 19:24:31 +0530

    changed view all button colour

--- src/components/common/ViewAllLinkButton.tsx
+++ src/components/common/ViewAllLinkButton.tsx
@@ -15,11 +15,9 @@ export default function ViewAllLinkButton({
   return (
     <Link
       href={link}
-      className={`inline-flex h-[35px] w-[100px] items-center justify-center gap-2 rounded-lg bg-white px-2 py-2 text-xs font-medium text-gray-700 shadow-sm transition-colors duration-200 hover:border-gray-400 hover:bg-gray-50 sm:h-[43px] sm:w-[120px] sm:text-sm md:border md:border-gray-300 ${className}`}
+      className={`inline-flex h-[35px] w-[100px] items-center justify-center gap-2 rounded-lg px-2 py-2 text-xs font-medium text-yellow shadow-sm transition-colors duration-200 hover:border-yellow hover:bg-gray-50 active:text-amber-500 sm:h-[43px] sm:w-[120px] sm:text-sm sm:text-gray-700 md:border md:border-gray-300 md:hover:text-yellow ${className}`}
     >
-      <span className="pb-[0.8rem] text-yellow sm:pb-0 md:text-text-primary">
-        {text}
-      </span>
+      <span className="pb-[0.8rem] sm:pb-0">{text}</span>
       <FaExternalLinkAlt className="hidden h-3 w-3 md:block" />
     </Link>
   );
