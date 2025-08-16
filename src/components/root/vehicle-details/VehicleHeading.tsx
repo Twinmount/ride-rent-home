208457afbe645b5cdc01313c4b1d817056544b29
Ajesh <ajeshs.dev@gmail.com>
2025-08-16 20:31:55 +0530

    feat: vehicle details page heading and breadcrump updated

---
+++ src/components/root/vehicle-details/VehicleHeading.tsx
@@ -0,0 +1,41 @@
+import BrandImage from '@/components/common/BrandImage';
+import Link from 'next/link';
+
+type Props = {
+  brandListingPageHref: string;
+  category: string;
+  brandValue: string;
+  heading: string;
+};
+
+export default function VehicleHeading({
+  brandListingPageHref,
+  category,
+  brandValue,
+  heading,
+}: Props) {
+  return (
+    <div className="mb-4 flex items-center gap-2 lg:gap-4">
+      {/* brand logo */}
+      <Link href={brandListingPageHref} className="h-fit w-fit">
+        <div className="h-12 w-12 min-w-12 cursor-pointer overflow-hidden rounded-full border bg-white p-1 hover:border-accent-light md:h-16 md:w-16 md:min-w-16 lg:h-[4.5rem] lg:w-[4.5rem] lg:min-w-[4.5rem]">
+          <BrandImage
+            category={category}
+            brandValue={brandValue}
+            className="h-full w-full object-contain"
+          />
+        </div>
+      </Link>
+
+      {/* <h1 className="custom-heading model-name">{heading}</h1> */}
+      <div className="h-fit space-y-1">
+        <h1 className="font-poppins text-lg font-medium leading-[1] text-text-primary md:text-2xl lg:text-3xl">
+          {heading}
+        </h1>
+        <p className="text-xs text-text-tertiary md:text-sm">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+    </div>
+  );
+}
