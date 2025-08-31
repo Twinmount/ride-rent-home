2bc8386bfa1e779847c00ec779a9da0303c88aaa
adithyan7053 <adithyan7053@gmail.com>
2025-08-31 19:57:34 +0530

    capitalized state in details page and updated dynamic content

--- src/components/root/vehicle-details/profile-specifications/VehicleStats.tsx
+++ src/components/root/vehicle-details/profile-specifications/VehicleStats.tsx
@@ -5,17 +5,23 @@ type VehicleStatsProps = {
 };
 
 const VehicleStats = ({ state }: VehicleStatsProps) => {
+  // Capitalize the first letter of each word in the state
+  const capitalizedState = state
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+
   return (
     <div className="flex items-center p-1 text-sm font-light text-text-secondary">
       <p className="mr-1">
         <CiLocationOn />
       </p>
-      <p>{state}</p>
+      <p>{capitalizedState}</p>
       {/* <p className="mx-4">|</p>
         <p className="">4.4  </p>
         <p className="ml-1 text-xs">⭐</p> */}
     </div>
   );
-}
+};
 
-export default VehicleStats
+export default VehicleStats;
