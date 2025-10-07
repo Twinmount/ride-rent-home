bd89cc70f5dac24f2298a28ea4ad9f99db7d64e5
adithyan7053 <adithyan7053@gmail.com>
2025-10-07 11:20:36 +0530

    updated rent now button

--- src/components/root/vehicle-details/profile-card/mobile-profile-card/MobileProfileCard.tsx
+++ src/components/root/vehicle-details/profile-card/mobile-profile-card/MobileProfileCard.tsx
@@ -4,24 +4,30 @@ import RentNowButtonWide from "@/components/common/RentNowbuttonWide";
 type MobileProfileCardProps = {
   profileData: ProfileCardDataType;
   country: string;
+  vehicle: any; // Add this prop
 };
 
 const MobileProfileCard = ({
   profileData,
   country,
+  vehicle, // Add this parameter
 }: MobileProfileCardProps) => {
-  // Destructure the needed values from profileData
   const {
     company,
     vehicleData: { state, model },
+    vehicleId, // Destructure these
+    agentId, // from profileData
   } = profileData;
 
   return (
     <>
-      {/* Mobile Profile Card - Simple Layout */}
       <div className="fixed bottom-2 z-20 w-[96%] pr-4 lg:hidden">
         <div>
           <RentNowButtonWide
+            vehicle={vehicle}
+            vehicleId={vehicleId}
+            agentId={agentId}
+            country={country}
             contactDetails={company.contactDetails}
             vehicleName={model}
             state={state}
